a7e51dcf93aa4a0fea872f472fa47c292a2f7ed7
LENOVO <LENOVO@10.20.3.240>
2018-04-05 17:04:41 +0530

    remove cooment new branch

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -23,7 +23,7 @@ public abstract class BaseTest implements Autoconst {
 		driver.get("https://www.betteru.in/");
 	}
 	
-	//before method 2
+	//before method 
 	@BeforeMethod	
 	public void preCondition1() {
 		System.setProperty(CHROME_KEY,CHROME_VALUE);
